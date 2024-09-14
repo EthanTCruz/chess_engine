98debc0d52444e477fd3e05f094f3601691ee118
Ethan Cruz <ethancruz3141@gmail.com>
2024-09-13 23:06:23 -0500

    minor change to pgn processor

--- src/model/classes/pgn_processor.py
+++ src/model/classes/pgn_processor.py
@@ -14,11 +14,11 @@ class pgn_processor():
     
 
 
-    def pgn_fen_to_sqlite(self,db: Session = SessionLocal()):
+    def pgn_fen_to_sqlite(self, db: Session = SessionLocal()):
         for filename in os.listdir(self.pgn_file):
-            file = f"{self.pgn_file}{filename}"
+            file = f"{self.pgn_file}/{filename}"
             total_games = count_games_in_pgn(pgn_file=file)
-            with open(file) as pgn:
+            with open(file, encoding='ISO-8859-1') as pgn:  # Specify the encoding
                 for _ in tqdm(range(total_games), desc=f"Processing {filename} Games to DB"):
                     game = chess.pgn.read_game(pgn)
 
@@ -38,7 +38,6 @@ class pgn_processor():
                     else:
                         print(game.headers["Result"])
                         raise Exception("No winner")
-                    
 
                     for move in game.mainline_moves():
                         board.push(move=move)
@@ -46,9 +45,10 @@ class pgn_processor():
                     insert_bulk_boards_into_db(board_victors=board_victors, db=db)
 
 
+
 def count_games_in_pgn(pgn_file):
     count = 0
-    with open(pgn_file) as pgn:
+    with open(pgn_file, encoding='ISO-8859-1') as pgn:  # Specify the encoding
         while chess.pgn.read_game(pgn) is not None:
             count += 1
     return count
